8ff8df1f477f3afbae4280ce827b1a942cdb4d75
duytnh <duytnh201@gmail.com>
2024-06-09 11:59:22 +0700

    edit validate

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -9,7 +9,7 @@ export const validateUsername = (username) => {
 export const validatePassword = (password) => {
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     if (!passwordRegex.test(password)) {
-        return 'Mật khẩu phải có ít nhất 1 chữ hoa, 1 chữ thường, 1 số, 1 ký tự đặc biệt và từ 8 ký tự trở lên.';
+        return 'Mật khẩu phải có ít nhất 1 chữ hoa, 1 chữ thường, 1 số, 1 ký tự đặc biệt và từ 8 ký tự trở lên';
     }
     return '';
 };
@@ -17,7 +17,7 @@ export const validatePassword = (password) => {
 export const validateEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-        return 'Email không hợp lệ. Vui lòng nhập địa chỉ email hợp lệ.';
+        return 'Email không hợp lệ. Vui lòng nhập địa chỉ email hợp lệ';
     }
     return '';
 };
